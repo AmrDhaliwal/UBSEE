4838a52e260325092e70735212202f6d97ee8b20
Amrit Dhaliwal <amrit97@student.ubc.ca>
2024-09-06 23:26:14 -0700

    Project is born.

---
+++ USBEE-Driver/Trace.h
@@ -0,0 +1,63 @@
+/*++
+
+Module Name:
+
+    Trace.h
+
+Abstract:
+
+    This module contains the local type definitions for the
+    driver.
+
+Environment:
+
+    Windows User-Mode Driver Framework 2
+
+--*/
+
+//
+// Device Interface GUID
+// 25629033-45ae-4173-b471-161787876d6d
+//
+DEFINE_GUID(GUID_DEVINTERFACE_USBEE-Driver,
+    0x25629033,0x45ae,0x4173,0xb4,0x71,0x16,0x17,0x87,0x87,0x6d,0x6d);
+
+//
+// Define the tracing flags.
+//
+// Tracing GUID - c8094777-eb71-45b9-a2e3-f28af0790ce7
+//
+
+#define WPP_CONTROL_GUIDS                                              \
+    WPP_DEFINE_CONTROL_GUID(                                           \
+        MyDriver1TraceGuid, (c8094777,eb71,45b9,a2e3,f28af0790ce7), \
+                                                                            \
+        WPP_DEFINE_BIT(MYDRIVER_ALL_INFO)                              \
+        WPP_DEFINE_BIT(TRACE_DRIVER)                                   \
+        WPP_DEFINE_BIT(TRACE_DEVICE)                                   \
+        WPP_DEFINE_BIT(TRACE_QUEUE)                                    \
+        )                             
+
+#define WPP_FLAG_LEVEL_LOGGER(flag, level)                                  \
+    WPP_LEVEL_LOGGER(flag)
+
+#define WPP_FLAG_LEVEL_ENABLED(flag, level)                                 \
+    (WPP_LEVEL_ENABLED(flag) &&                                             \
+     WPP_CONTROL(WPP_BIT_ ## flag).Level >= level)
+
+#define WPP_LEVEL_FLAGS_LOGGER(lvl,flags) \
+           WPP_LEVEL_LOGGER(flags)
+               
+#define WPP_LEVEL_FLAGS_ENABLED(lvl, flags) \
+           (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
+
+//
+// This comment block is scanned by the trace preprocessor to define our
+// Trace function.
+//
+// begin_wpp config
+// FUNC Trace{FLAG=MYDRIVER_ALL_INFO}(LEVEL, MSG, ...);
+// FUNC TraceEvents(LEVEL, FLAGS, MSG, ...);
+// end_wpp
+//
+
